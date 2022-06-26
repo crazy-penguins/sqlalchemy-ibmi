6ae81e5a04be86c1b2173de77cd24f02f160757b
--disabled-password <pshingavi@gmail.com>
2022-06-26 09:02:33 -0500

    Bump version: 0.9.3-dev → 0.9.3-rc

--- sqlalchemy_ibmi/__init__.py
+++ sqlalchemy_ibmi/__init__.py
@@ -19,6 +19,6 @@
 # | Contributors: Jaimy Azle, Mike Bayer                                     |
 # +--------------------------------------------------------------------------+
 
-__version__ = '0.9.3-dev'
+__version__ = '0.9.3-rc'
 
 from . import base  # noqa: F401
